a1e480c6fcb3a6466b05f7b7899e3b2c6952b557
Kieran <mcclymont.kieran@gmail.com>
2018-04-26 10:51:51 -0300

    Changed the way the line moves in drag and drop and updated the slide bar to be consistant. Starting to add pie chart

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import * as d3 from "d3"
-
+const grey = 'rgb(128, 158, 155)'
 
 class DragDrop extends Component {
   constructor() {
@@ -10,36 +10,35 @@ class DragDrop extends Component {
 
   componentDidMount() {
     let mainContainer = d3.select(this.refs.svgContainer)
+
+    // Append starting line
+    mainContainer.append('line')
+    .style('stroke', 'black')
+    .attr('x1', 48 + 10)
+    .attr('y1', 168 + 10)
+    .attr('x2', 138)
+    .attr('y2', 50)
+
     mainContainer.append('rect')
-    .attr('width', '20px')
-    .attr('height', '20px')
-    .attr('x', 48)
-    .attr('y', 168)
-    .style('fill', 'green')
+      .attr('width', '20px')
+      .attr('height', '20px')
+      .attr('x', 48)
+      .attr('y', 168)
+      .style('fill', 'red')
 
     mainContainer.append('circle')
       .attr('r', 10)
       .attr('cx', 138)
       .attr('cy', 50)
-      .style('fill', 'green')
-
-    mainContainer.append('line')
-      .style('stroke', 'green')
-      .attr('x1', d3.select('circle').attr('cx'))
-      .attr('y1', d3.select('circle').attr('cy'))
-      .attr('x2', Number(d3.select('rect').attr('x')) + 10)
-      .attr('y2', Number(d3.select('rect').attr('y')) + 10)
-
+      .style('fill', 'red')
 
     d3.select('rect').call(d3.drag()
       .on('drag', function() {
         let coordinates = d3.mouse(this)
         let cx = coordinates[0]
         let cy = coordinates[1]
-        mainContainer.selectAll('line').remove()
-        mainContainer.append('line')
-          .transition()
-          .style('stroke', 'green')
+        mainContainer.selectAll('line')
+          .transition().delay(0).duration(1)
           .attr('x1', d3.select('circle').attr('cx'))
           .attr('y1', d3.select('circle').attr('cy'))
           .attr('x2', Number(d3.select('rect').attr('x')) + 10)
@@ -50,28 +49,22 @@ class DragDrop extends Component {
         })
       )
 
-
     d3.select('circle').call(d3.drag()
       .on('drag', function() {
         let coordinates = d3.mouse(this)
         let cx = coordinates[0]
         let cy = coordinates[1]
-      mainContainer.selectAll('line').remove()
-      mainContainer.append('line')
-        .transition()
-        .style('stroke', 'green')
-        .attr('x1', d3.select('circle').attr('cx'))
-        .attr('y1', d3.select('circle').attr('cy'))
-        .attr('x2', Number(d3.select('rect').attr('x')) + 10)
-        .attr('y2', Number(d3.select('rect').attr('y')) + 10)
-      d3.select('circle')
-        .attr('cx', cx)
-        .attr('cy', cy)
+        mainContainer.selectAll('line')
+          .attr('x1', d3.select('circle').attr('cx'))
+          .attr('y1', d3.select('circle').attr('cy'))
+          .attr('x2', Number(d3.select('rect').attr('x')) + 10)
+          .attr('y2', Number(d3.select('rect').attr('y')) + 10)
+        d3.select('circle')
+          .attr('cx', cx)
+          .attr('cy', cy)
       })
     )
   }
-
-
 
   render() {
     return(
