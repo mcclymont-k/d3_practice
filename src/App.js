f80f5460bb817b7a3e82e3fff53cf49119991595
Kieran <mcclymont.kieran@gmail.com>
2018-05-01 10:39:34 -0300

    Updated the brush so that it changes the underlying fill of the circles

--- src/App.js
+++ src/App.js
@@ -12,18 +12,27 @@ class App extends Component {
         <TopBar />
         <div className="mainContainer">
           <h1>Drag and drop</h1>
-          <h2>A d3 svg that allows you to drag and drop the rectangle and circle. The line will always follow</h2>
+          <h2>
+            A d3 svg that allows you to drag and drop the rectangle and circle.
+            The line will always follow
+          </h2>
           <DragDrop />
           <div className='spacer'></div>
           <h1>Pie Chart</h1>
-          <h2>This pie chart is pulling data from an api and calaculating the
-          number of charachters in a random selection of peoples names.
-          On hover it shows what the name length of the category
-          is and the wedge size shows the quantity of people in that category</h2>
+          <h2>
+            This pie chart is pulling data from an api and calaculating the
+            number of charachters in a random selection of peoples names.
+            On hover it shows what the name length of the category
+            is and the wedge size shows the quantity of people in that category
+          </h2>
           <PieChart />
           <div className='spacer'></div>
-          <h1>Other Chart</h1>
-          <h2>An other thing</h2>
+          <h1>Brush chart</h1>
+          <h2>
+            This brush chart places random data on the graph and then uses the
+            brusher to change the styling of the underlying data. Click and drag
+            the brusher to see the change
+          </h2>
           <Brush />
         </div>
       </div>
--- src/components/Brush.js
+++ src/components/Brush.js
@@ -11,36 +11,48 @@ class Brush extends Component {
   componentDidMount() {
     let randomX = d3.randomUniform(10, 590)
     let randomY = d3.randomUniform(180, 220)
-    let mainData = d3.range(400).map(() => [randomX(), randomY()])
+    let mainData = d3.range(200).map(() => [randomX(), randomY()])
     this.setState({mainData: mainData})
   }
 
   componentDidUpdate() {
-    let brushContainer = d3.select(this.refs.brushContainer)
-    let brushX = d3.brushX()
+    let mainContainer = d3.select(this.refs.brushContainer)
+    let brush = d3.brushX()
 
-    brushContainer.append('line')
+    mainContainer.append('line')
       .style('stroke', 'black')
       .attr('x1', 0)
       .attr('y1', 200)
       .attr('x2', 600)
       .attr('y2', 200)
 
-    brushContainer.append('g')
+    mainContainer.append('g')
       .attr('class', 'brush')
       .selectAll('circle')
         .data(this.state.mainData)
         .enter().append('circle')
+          .attr('class', 'circle')
           .attr('r', 3)
-          .style('fill', 'red')
+          .style('fill', 'grey')
           .attr('fill-opacity', 0.5)
           .attr('transform', (d) => 'translate(' + d[0] + ',' + d[1] + ')')
 
-    let brush = d3.select('.brush')
-    brush.call(brushX.extent([[0,140], [600, 260]]))
-    brushX.move(brush, [0,50])
+    let brushContainer = d3.select('.brush')
+    brushContainer.call(brush.extent([[0,140], [600, 260]]))
+    brush.move(brushContainer, [0,50])
 
-    brushX.on('brush', () => console.log(d3.event))
+    brush.on('brush', (d) => {
+      let brushPosition = d3.select('.selection').attr('x')
+      let circlesSelector = document.querySelectorAll('.circle')
+      circlesSelector.forEach(circle => {
+        let eachCircleX = circle.__data__[0]
+        if (eachCircleX > brushPosition && eachCircleX < (Number(brushPosition) + 50)) {
+          d3.select(circle).style('fill', 'blue')
+        } else {
+          d3.select(circle).style('fill', 'grey')
+        }
+      })
+    })
   }
 
   render() {
